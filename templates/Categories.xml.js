3f2b52a1c503921bbbfed11feeb407d45f6147a8
nathanxsnow <noxunit.com>
2020-06-04 16:54:45 +0200

    144

--- templates/Categories.xml.js
+++ templates/Categories.xml.js
@@ -37,7 +37,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
           <title class="showTextOnHighlight">Dance Wave</title>
         </lockup>
         <lockup Id="IbizaGlobal">
-          <img src="http://data.radiomyme.com/tv/resources/images/radio/ibizaglobalradio.jpg" width="308" height="308" />
+          <img src="https://abload.de/img/12040191-960464833704dfkn3.jpg" width="308" height="308" />
           <title class="showTextOnHighlight">Ibiza Global Radio</title>
         </lockup>
          <lockup Id="SunshineLive">
