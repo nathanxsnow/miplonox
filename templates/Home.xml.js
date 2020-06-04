98e8afe2afb80dbaf1148f71b65a28a3ff6b94f7
nathanxsnow <noxunit.com>
2020-06-04 22:09:54 +0200

    tzzzz

--- templates/Home.xml.js
+++ templates/Home.xml.js
@@ -24,7 +24,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
       text-align: center;
     }
     .templateBackground {
-          background-color: #20092a;
+          background-color: #091a2a;
     }
     .carouselOverlay {
 				padding:180 90 0 1100;
