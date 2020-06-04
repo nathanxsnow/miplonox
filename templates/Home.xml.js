e64927d82cc6346b273f3ce1daf9a90d1e19e184
nathanxsnow <noxunit.com>
2020-06-04 15:57:00 +0200

    lo1

--- templates/Home.xml.js
+++ templates/Home.xml.js
@@ -122,11 +122,11 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
     
       <shelf>
         <header>
-          <title>Editor's choice</title>
+          <title>Action Movies</title>
         </header>
         <section>
         <lockup Id="NRJ">
-            <img src="https://radiomyme.com/tv/resources/images/radio/NRJ.jpg" width="308" height="308" />
+            <img src="https://abload.de/img/12040191-960464833704dfkn3.jpg" width="308" height="308" />
             <title class="showTextOnHighlight">NRJ</title>
         </lockup>
         <lockup Id="DanceWave">
