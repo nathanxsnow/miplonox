aa874f709420356b99badd965964c8912d6d922a
nathanxsnow <noxunit.com>
2020-06-05 15:12:57 +0200

    jfgfe4521

--- templates/Categories.xml.js
+++ templates/Categories.xml.js
@@ -33,15 +33,15 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
               <grid>
                 <section>
         <lockup Id="ul1">
-          <img src="http://data.radiomyme.com/tv/resources/images/radio/DanceWave.jpg" width="308" height="308" />
+          <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
           <title class="showTextOnHighlight">Dance Wave</title>
         </lockup>
         <lockup Id="ul2">
-          <img src="http://data.radiomyme.com/tv/resources/images/radio/ibizaglobalradio.jpg" width="308" height="308" />
+          <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
           <title class="showTextOnHighlight">Ibiza Global Radio</title>
         </lockup>
          <lockup Id="ul3">
-          <img src="http://data.radiomyme.com/tv/resources/images/radio/sunshinelive.jpg" width="308" height="308" />
+          <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
           <title class="showTextOnHighlight">Sunshine Live</title>
         </lockup>
                 </section>
@@ -55,15 +55,15 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
               <grid>
                 <section>
         <lockup Id="hab1">
-            <img src="http://data.radiomyme.com/tv/resources/images/radio/NRJ.jpg" width="308" height="308" />
+            <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
             <title class="showTextOnHighlight">NRJ</title>
         </lockup>
         <lockup Id="hab2">
-            <img src="http://data.radiomyme.com/tv/resources/images/radio/DanceWave.jpg" width="308" height="308" />
+            <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
             <title class="showTextOnHighlight">Dance Wave</title>
         </lockup>
         <lockup Id="hab3">
-            <img src="http://data.radiomyme.com/tv/resources/images/radio/hotmix.jpg" width="308" height="308" />
+            <img src="https://abload.de/img/live-tv3lj43.jpg" width="308" height="308" />
             <title class="showTextOnHighlight">HotMix Radio Dance</title>
         </lockup>
                 </section>
