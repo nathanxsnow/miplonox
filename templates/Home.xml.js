f47852aae1a3bec8471d08d16d746ee9154c4117
nathanxsnow <noxunit.com>
2020-06-08 14:50:15 +0200

    klklk

--- templates/Home.xml.js
+++ templates/Home.xml.js
@@ -104,24 +104,7 @@ var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
     </style>
   </head>
   
-  <stackTemplate class="templateBackground" theme="dark">
-    <collectionList>  
-      <carousel>
-        <section>
-		<lockup Id="act1">
-            <img src="https://abload.de/img/tomnew7nkm3.jpg" width="1540" height="450" />
-        </lockup>
-        <lockup Id="act2">
-            <img src="https://abload.de/img/ramnewfajx7.jpg" width="1540" height="450" />
-        </lockup>
-        <lockup Id="act3">
-            <img src="https://abload.de/img/rewnewx4juh.jpg" width="1540" height="450" />
-        </lockup>
-        <lockup Id="act4">
-            <img src="https://abload.de/img/teri5qkcd.jpg" width="1540" height="450" />
-        </lockup>
-        </section>
-      </carousel>
+  
     
       <shelf>
         <header>
